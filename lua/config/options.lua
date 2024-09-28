943486e0592f5e9c6449024017ca83d6badc3506
suyashLade12 <suyash.lade@smallcase.com>
2024-09-29 00:12:32 +0530

    updating condition for windows condition

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options hereb 
-vim.opt.relativenumber = false-- Relative line numberichost
-LazyVim.terminal.setup("pwsh")
+-- Add any additional options hereb
+vim.opt.relativenumber = false -- Relative line numberichost
+if vim.env.TERM == "windows" then
+  LazyVim.terminal.setup("pwsh")
+end
