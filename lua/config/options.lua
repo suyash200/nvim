1d0f87e92182f39b57eb1f7f96ddca5255ba0caf
suyashLade12 <suyashlade@gmail.com>
2024-09-29 00:14:19 +0530

    fixed it

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options hereb
 vim.opt.relativenumber = false -- Relative line numberichost
-if vim.env.TERM == "windows" then
+if vim.env.TERM ~= "linux" then
   LazyVim.terminal.setup("pwsh")
 end
