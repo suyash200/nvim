7f5555e565486ffb787044a05f6660a77af693eb
suyashLade12 <suyashlade@gmail.com>
2024-09-28 23:59:40 +0530

    adding pwsh as terminal dk abt the linux implications tbh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options hereb 
-vim.opt.relativenumber = false-- Relative line numbers
+vim.opt.relativenumber = false-- Relative line numberichost
+LazyVim.terminal.setup("pwsh")
