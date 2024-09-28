b9fd791a2b286a4bb0ed978c7b5399223fcdee1c
suyashLade12 <suyashlade@gmail.com>
2024-09-28 23:50:07 +0530

    updating colourScheme and opt updates

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+
+
+return {
+  -- add gruvbox
+  { "rose-pine/neovim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-moon",
+    },
+  }
+}
